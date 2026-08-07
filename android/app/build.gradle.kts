5d34b90df256e259e2ed7f36428bb58569267eeb
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2026-08-07 01:49:12 -0400

    chore(config): update compileSdk to 37, add dependencies and intent filters

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.example.chat_stats"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
